d2d1720ee408070bdcb77274325fc8b056fb409b
Josh Calder <8251494+borisno2@users.noreply.github.com>
2025-11-04 11:14:34 +1100

    Remove unneeded mcp dep from cli (#63)
    * remove unneeded mcp dep from cli
    
    * changesets

--- examples/starter/next.config.js
+++ examples/starter/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['@prisma/client', '@opensaas/stack-core', '@opensaas/stack-ui'],
+  serverExternalPackages: ['@prisma/client'],
+  transpilePackages: ['@opensaas/stack-ui', '@opensaas/stack-core', '@opensaas/stack-auth'],
 }
 // eslint-disable-next-line no-undef
 module.exports = nextConfig
